f694170607fd76b0a74d847ab1f124ecb51c9789
Jacob Kanipe-Illig <jacobhyphenated@gmail.com>
2022-12-03 14:10:10 -0500

    day3 cleanup

--- src/main/kotlin/com/jacobhyphenated/advent2022/day3/Day3.kt
+++ src/main/kotlin/com/jacobhyphenated/advent2022/day3/Day3.kt
@@ -2,8 +2,15 @@ package com.jacobhyphenated.advent2022.day3
 
 import com.jacobhyphenated.advent2022.Day
 
+/**
+ * Day 3: Rucksack Reorganization
+ *
+ * There are a bunch of rucksacks with different items (characters a-z and A-Z)
+ * Each item has a priority: a-z is 1-26 and A-Z is 27-52
+ */
 class Day3: Day<List<String>> {
 
+    // create a map that ties each item character to its priority score
     private val  priorityMap = (
             ('a'..'z').zip(1..26) +
             ('A'..'Z').zip(27..52)
@@ -13,8 +20,12 @@ class Day3: Day<List<String>> {
         return readInputFile("day3").lines()
     }
 
+    /**
+     * Each rucksack is evenly split into two sections.
+     * For each rucksack, the two sections will have 1 item in common.
+     * Find that item's priority and sum the priority score for all rucksacks.
+     */
     override fun part1(input: List<String>): Number {
-
         return input.sumOf {
             val half = it.length / 2
             val firstHalf = it.substring(0, half).toCharArray().toSet()
@@ -24,10 +35,17 @@ class Day3: Day<List<String>> {
         }
     }
 
+    /**
+     * The elves have groups of three (in order based on the puzzle input).
+     * Each group has exactly one item in all 3 rucksacks.
+     *
+     * Find the priority of the unique item for each group, and add them all up.
+     */
     override fun part2(input: List<String>): Number {
+        // use windowed to get groups of 3 with a step of 3 between each group so no rucksack appears in multiple groups
         return input.windowed(3, 3).sumOf { rucksack ->
             rucksack.map { it.toCharArray().toSet() }
-                .reduce { a, b -> a.intersect(b) }
+                .reduce { a, b -> a.intersect(b) }// set intersection to find the common item in rucksack group
                 .sumOf { priorityMap.getValue(it) }
         }
     }
