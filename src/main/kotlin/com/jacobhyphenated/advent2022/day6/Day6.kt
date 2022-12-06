64b4432c67959650dbba7a4382413b30e82821d0
Jacob Kanipe-Illig <jacobhyphenated@gmail.com>
2022-12-06 10:38:00 -0500

    re-write day6 with map/reduce
    also add compiler warm up function to the Day interface

--- src/main/kotlin/com/jacobhyphenated/advent2022/day6/Day6.kt
+++ src/main/kotlin/com/jacobhyphenated/advent2022/day6/Day6.kt
@@ -2,32 +2,49 @@ package com.jacobhyphenated.advent2022.day6
 
 import com.jacobhyphenated.advent2022.Day
 
+/**
+ * Day 6: Tuning Trouble
+ *
+ * The malfunctioning communicator receives characters in order (puzzle input)
+ * These characters make up a packet of information.
+ */
 class Day6: Day<String> {
     override fun getInput(): String {
         return readInputFile("day6")
     }
 
-    override fun part1(input: String): Number {
-        // starts the index of the packet marker window at 2+1 == 4th character
-        var index = 2
-        val marker = mutableSetOf<Char>()
-        while (marker.size != 4) {
-            index++
-            marker.clear()
-            marker.addAll(arrayOf(input[index], input[index-1], input[index-2], input[index-3]))
-        }
-        return index + 1
+    override fun warmup(input: String): Any {
+        // teach the compiler how to optimize for this function
+        return findUniqueMarker(input, 2)
     }
 
+    /**
+     * The packet start is denoted by 4 different consecutive characters.
+     * How many characters have arrived when the packet start is identified?
+     */
+    override fun part1(input: String): Number {
+        return findUniqueMarker(input, 4)
+    }
+
+    /**
+     * The packet message consists of 14 unique consecutive characters
+     * How many characters have arrived before you can read the packet message?
+     */
     override fun part2(input: String): Number {
-        val packet = input.toCharArray()
-        var index = 12
-        val marker = mutableSetOf<Char>()
-        while (marker.size != 14) {
-            index++
-            marker.clear()
-            marker.addAll(packet.slice(index - 13 .. index))
-        }
-        return index + 1
+        return findUniqueMarker(input, 14)
+    }
+
+    /**
+     * Both parts are solved with the same algorithm.
+     * Use a marker size to look at a moving window of characters
+     * If a set of those characters is equal to the marker size, all characters are unique
+     *
+     * indexOfFirst gives the index at the beginning of the window. Add the marker size
+     * to find the total number of characters in the packet so far
+     */
+    private fun findUniqueMarker(input: String, markerSize: Int): Number {
+        return markerSize + input.toList()
+            .windowed(markerSize)
+            .indexOfFirst { it.toSet().size == markerSize }
     }
 }
