85a28f944d94b3b8fb8e78f9a1b0b64201abd44b
Jacob Kanipe-Illig <jacobhyphenated@gmail.com>
2022-12-09 16:09:17 -0500

    day 9 cleanup

--- src/main/kotlin/com/jacobhyphenated/advent2022/day9/Day9.kt
+++ src/main/kotlin/com/jacobhyphenated/advent2022/day9/Day9.kt
@@ -3,24 +3,39 @@ package com.jacobhyphenated.advent2022.day9
 import com.jacobhyphenated.advent2022.Day
 import kotlin.math.absoluteValue
 
+/**
+ * Day 9: Rope Bridge
+ *
+ * Knots on a rope follow a particular pattern that can be represented in 2d space.
+ * The tail knot must always be adjacent (including diagonals) to the head knot.
+ * As the head moves, the tail follows:
+ *      ...   ->   ...   ->   ...
+ *      TH.        T.H        .TH
+ * and
+ *      ...   ->   .H.   ->   .H.
+ *      .H.        ...        .T.
+ *      T..        T..        ...
+ */
 class Day9: Day<List<Pair<Direction, Int>>> {
     override fun getInput(): List<Pair<Direction, Int>> {
         return parseInput(readInputFile("day9"))
     }
 
-    override fun part1(input: List<Pair<Direction, Int>>): Any {
+    override fun warmup(input: List<Pair<Direction, Int>>): Int {
+        return part1(input) + part2(input)
+    }
+
+    /**
+     * Given the input instructions which describe how the head knot moves,
+     * How many unique spaces does the tail knot visit?
+     */
+    override fun part1(input: List<Pair<Direction, Int>>): Int {
         var head = Pair(0,0)
         var tail = Pair(0,0)
         val visited = mutableSetOf(Pair(0,0))
         input.forEach { (direction, distance) ->
             repeat(distance) {
-                val (x,y) = head
-                head = when (direction) {
-                    Direction.DOWN -> Pair(x, y - 1)
-                    Direction.UP -> Pair(x, y + 1)
-                    Direction.LEFT -> Pair(x - 1, y)
-                    Direction.RIGHT -> Pair(x + 1, y)
-                }
+                head = updatePosition(head, direction)
                 tail = calcNewTailPosition(head, tail)
                 visited.add(tail)
             }
@@ -28,18 +43,17 @@ class Day9: Day<List<Pair<Direction, Int>>> {
         return visited.size
     }
 
-    override fun part2(input: List<Pair<Direction, Int>>): Any {
+    /**
+     * The rope actually has 10 knots. The head knot moves as before.
+     * Each knot in the rope acts like the tail knot to the knot in front of it.
+     * How many unique spaces does the last tail know move through?
+     */
+    override fun part2(input: List<Pair<Direction, Int>>): Int {
         val rope = MutableList(10) { Pair(0,0) }
         val visited = mutableSetOf(Pair(0,0))
         input.forEach { (direction, distance) ->
             repeat(distance) {
-                val (x,y) = rope[0]
-                rope[0] = when (direction) {
-                    Direction.DOWN -> Pair(x, y - 1)
-                    Direction.UP -> Pair(x, y + 1)
-                    Direction.LEFT -> Pair(x - 1, y)
-                    Direction.RIGHT -> Pair(x + 1, y)
-                }
+                rope[0] = updatePosition(rope[0], direction)
                 for (i in 1 until rope.size) {
                     rope[i] = calcNewTailPosition(rope[i-1], rope[i])
                 }
@@ -48,6 +62,16 @@ class Day9: Day<List<Pair<Direction, Int>>> {
         }
         return visited.size
 
+    }
+
+    private fun updatePosition(position: Pair<Int,Int>, direction: Direction): Pair<Int,Int> {
+        val (x,y) = position
+        return when (direction) {
+            Direction.DOWN -> Pair(x, y - 1)
+            Direction.UP -> Pair(x, y + 1)
+            Direction.LEFT -> Pair(x - 1, y)
+            Direction.RIGHT -> Pair(x + 1, y)
+        }
     }
 
     private fun calcNewTailPosition(head: Pair<Int,Int>, tail: Pair<Int,Int>): Pair<Int,Int> {
